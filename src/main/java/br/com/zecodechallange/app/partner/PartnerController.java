1e394a715750fffd998c4ae9de1a6a4d8e3b3e32
Gabiroba <gabrielcardosogirarde@gmail.com>
2024-04-20 21:41:41 -0300

    feat: README

--- src/main/java/br/com/zecodechallange/app/partner/PartnerController.java
+++ src/main/java/br/com/zecodechallange/app/partner/PartnerController.java
@@ -3,7 +3,6 @@ package br.com.zecodechallange.app.partner;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
